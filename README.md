b41c6782a4a008df3256d1544a6f587e214491b2
mgechev <mgechev@gmail.com>
2014-04-03 11:18:13 +0300

    Update project confituration

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-dyslexio
+Dyslexio
 ========
+
+# How to deploy
+
+You need node.js, bower and gulp installed. When you resolve these dependencies enter root and:
+
+```shell
+npm install
+bower install
+gulp watch
+open http://localhost:55555/
+```
---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    livereload = require('gulp-livereload');
+
+gulp.task('staticsvr', function (next) {
+  var staticS = require('node-static'),
+      server = new staticS.Server('./'),
+      port = 55555;
+  require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+      server.serve(request, response);
+    }).resume();
+  }).listen(port, function () {
+    gutil.log('Server listening on port: ' + gutil.colors.magenta(port));
+    next();
+  });
+});
+
+gulp.task('watch', ['staticsvr'], function () {
+  var server = livereload();
+  gulp.watch('/**').on('change', function (file) {
+      server.changed(file.path);
+  });
+});
