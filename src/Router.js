35d822573ec52eeec6ec97be867a02f7f5091902
mgechev <mgechev@gmail.com>
2014-04-10 10:47:23 +0300

    Add route handling

---
+++ src/Router.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var Router = {
+  handlePage: function () {
+    console.log(arguments);
+  },
+  handleGame: function () {
+    console.log(arguments);
+  }
+};
--- src/app.js
+++ src/app.js
@@ -2,6 +2,15 @@
 
 'use strict';
 
+crossroads.addRoute('/', Router.handlePage);
+crossroads.addRoute('/about', Router.handlePage);
+crossroads.addRoute('/help', Router.handlePage);
+crossroads.addRoute('/game/{id}', Router.handleGame);
+$(window).hashchange(function () {
+  var hash = location.href.hash || '#';
+  crossroads.parse(hash.substring(1, hash.length));
+});
+
 Dyslexio.Models.GameFactory.init()
 .done(function (data) {
   Dyslexio.Views.SideNav.init(data);
