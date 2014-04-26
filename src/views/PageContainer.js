ce8f80e69caf822fad613f837cae46af8e1c9654
mgechev <mgechev@gmail.com>
2014-04-26 10:28:54 -0700

    Add jplayer

--- src/views/PageContainer.js
+++ src/views/PageContainer.js
@@ -28,6 +28,42 @@ Dyslexio.Views.PageContainer = {
     this.container.empty();
     this.container.append(iframe);
     iframe.attr('src', game.url);
+    $('#jquery_jplayer_1').jPlayer({
+      swfPath: 'src/lib/jplayer/jquery.jplayer/',
+      solution: 'html, flash',
+      supplied: 'm4a, oga',
+      preload: 'metadata',
+      volume: 0.8,
+      muted: false,
+      backgroundColor: '#000000',
+      cssSelectorAncestor: '#jp_container_1',
+      cssSelector: {
+        videoPlay: '.jp-video-play',
+        play: '.jp-play',
+        pause: '.jp-pause',
+        stop: '.jp-stop',
+        seekBar: '.jp-seek-bar',
+        playBar: '.jp-play-bar',
+        mute: '.jp-mute',
+        unmute: '.jp-unmute',
+        volumeBar: '.jp-volume-bar',
+        volumeBarValue: '.jp-volume-bar-value',
+        volumeMax: '.jp-volume-max',
+        playbackRateBar: '.jp-playback-rate-bar',
+        playbackRateBarValue: '.jp-playback-rate-bar-value',
+        currentTime: '.jp-current-time',
+        duration: '.jp-duration',
+        title: '.jp-title',
+        fullScreen: '.jp-full-screen',
+        restoreScreen: '.jp-restore-screen',
+        repeat: '.jp-repeat',
+        repeatOff: '.jp-repeat-off',
+        gui: '.jp-gui',
+        noSolution: '.jp-no-solution'
+      },
+      errorAlerts: false,
+      warningAlerts: false
+    });
   },
   renderGame: function (game) {
     var self = this;
