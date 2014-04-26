f65b60f9a2b458c47477ddd3f0cd15242cb06ece
mgechev <mgechev@gmail.com>
2014-04-26 10:47:00 -0700

    Update the player initialization

--- src/views/PageContainer.js
+++ src/views/PageContainer.js
@@ -28,46 +28,16 @@ Dyslexio.Views.PageContainer = {
     this.container.empty();
     this.container.append(iframe);
     iframe.attr('src', game.url);
-    $('#jp_container_1').jPlayer({
-      swfPath: 'src/lib/jplayer/jquery.jplayer/',
-      solution: 'html, flash',
-      supplied: 'm4a, oga',
-      preload: 'metadata',
-      volume: 0.8,
-      muted: false,
-      backgroundColor: '#000000',
-      cssSelectorAncestor: '#jp_container_1',
-      cssSelector: {
-        videoPlay: '.jp-video-play',
-        play: '.jp-play',
-        pause: '.jp-pause',
-        stop: '.jp-stop',
-        seekBar: '.jp-seek-bar',
-        playBar: '.jp-play-bar',
-        mute: '.jp-mute',
-        unmute: '.jp-unmute',
-        volumeBar: '.jp-volume-bar',
-        volumeBarValue: '.jp-volume-bar-value',
-        volumeMax: '.jp-volume-max',
-        playbackRateBar: '.jp-playback-rate-bar',
-        playbackRateBarValue: '.jp-playback-rate-bar-value',
-        currentTime: '.jp-current-time',
-        duration: '.jp-duration',
-        title: '.jp-title',
-        fullScreen: '.jp-full-screen',
-        restoreScreen: '.jp-restore-screen',
-        repeat: '.jp-repeat',
-        repeatOff: '.jp-repeat-off',
-        gui: '.jp-gui',
-        noSolution: '.jp-no-solution'
+    $('#jquery_jplayer_1').jPlayer({
+      ready: function () {
+        for (var audio in game.instructions) {
+          game.instructions[audio] = game.url + '/' + game.instructions[audio];
+        }
+        $(this).jPlayer('setMedia', game.instructions);
       },
-      errorAlerts: false,
-      warningAlerts: false
+      swfPath: '/js',
+      supplied: 'm4a, oga'
     });
-    for (var audio in game.instructions) {
-      game.instructions[audio] = game.url + '/' + game.instructions[audio];
-    }
-    $('#jp_container_1').jPlayer('setMedia', game.instructions);
   },
   renderGame: function (game) {
     var self = this;
