ace28f73bf5ea3de311b3e2754d313e619b31986
tlilyanova <tlilyanova@gmail.com>
2014-06-08 13:05:03 +0300

    Add difficulty level on punctuation game

--- games/punctuation/game.js
+++ games/punctuation/game.js
@@ -1,16 +1,35 @@
-var sentence = 'Сложи#запетая тук#и точка тук#';
-var game = 0;
-var c = 0;
-var limit = 1;
-var texts = ['text1.txt', 'text2.txt'];
-var punctSymbols = [',', '.', '?'];
-var correctSentences = ['Сложи запетая тук, и точка тук.', 'Сложи точка тук.'];
+var fileName = 'texts.txt';
+var punctSymbols = [',', '.', '?',';','!'];
+var text;
+var textsArray;
+var usedTexts = [];
+var countUsedTexts = [];
+var countOfTexts;
+var markUsed = [];
+var countLevels = 3;
+var indexOfText;
+var level;
+
+function init() {
+  'use strict';
+  var jsonData = JSON.parse($('#helpDiv').text());
+  textsArray = jsonData.texts;
+  countOfTexts = jsonData.countOfTextsForEachLevel;
+  var length = countOfTexts * countLevels;
+  while (length--) {
+	usedTexts.push(0);
+	if (length < countLevels) {
+	  countUsedTexts.push(0);
+	  markUsed.push(1);
+	}
+  }
+}
 
 function resetRound() {
   'use strict';
   $('#sentenceHolder').empty();
-  var sentenceDivHtml = sentence.replace(/#/g, '<span class="droppable">&nbsp;</span>');
-  $('#sentenceHolder').html(sentenceDivHtml);
+  $('#sentenceHolder').html(text);
+  
   $('.droppable').droppable({
     hoverClass: 'drop-hover',
     tolerance: 'touch',
@@ -22,41 +41,65 @@ function resetRound() {
 
 function startNewRound() {
   'use strict';
-  sentence = $('#helpDiv').text();
+  text = textsArray[indexOfText].replace(/\./g, '<span class="droppable">&nbsp;</span>')
+         .replace(/,/g, '<span class="droppable">&nbsp;</span>')
+         .replace(/\?/g, '<span class="droppable">&nbsp;</span>')
+		 .replace(/\!/g, '<span class="droppable">&nbsp;</span>')
+		 .replace(/\;/g, '<span class="droppable">&nbsp;</span>');
   $('#punctuationHolder').empty();
-  for (var i = 0; i < punctSymbols.length; i += 1) {
+  var len = punctSymbols.length + level - 2;
+  for (var i = 0; i < len; i += 1) {
     $('<span />',
       {text: punctSymbols[i], class: 'draggable'})
       .appendTo('#punctuationHolder');
   }
-  $('.draggable').draggable({revert: true});
+  $('.draggable').draggable({revert: true, revertDuration: 0});
   resetRound();
 }
 
-function loadText() {
+function chooseLevel(choosedLevel) {
   'use strict';
-  $('#helpDiv').load(texts[game], startNewRound);
-  c = game;
-  if (game === limit) {
-    game = 0;
-  } else {
-    game += 1;
+  level = choosedLevel;
+  indexOfText = getTextIndex(level);
+  startNewRound();
+}
+
+function getTextIndex(level) {
+  'use strict';
+  if (countUsedTexts[level] == countOfTexts) {
+	countUsedTexts[level] = 0;
+	markUsed[level] +=1;	
   }
+  var index;
+  while (true) {
+	index = randomNumber(level  * countOfTexts, (level + 1) * countOfTexts - 1);
+	if (usedTexts[index] != markUsed[level]) {
+	  usedTexts[index] = markUsed[level];
+	  countUsedTexts[level] +=1;
+	  break;
+	}
+  }
+  return index;
+}
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function startNewGame() {
   'use strict';
-  loadText();
+  indexOfText = getTextIndex(level);
+  startNewRound();
 }
 
 function checkSolution() {
   'use strict';
   var correct = $('#sentenceHolder').text()
-      .replace(/\u00a0/g, ' ')
-      .trim() === correctSentences[c];
+	  .replace(/\u00a0/g,' ')
+	  .trim() === textsArray[indexOfText]; 
   if (correct) {
     alert('Поздравления! Решихте правилно задачата');
-    loadText();
+    startNewGame();
   } else {
     alert('Имате грешки в решението. Опитайте отново');
   }
@@ -67,6 +110,6 @@ $(function () {
   $('#checkButton').click(checkSolution);
   $('#resetButton').click(resetRound);
   $('#newRoundButton').click(startNewGame);
-  startNewGame();
+  $('#helpDiv').load(fileName, init);
 });
 
