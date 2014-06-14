985d46ec5cff3fedfbd27d03ea07a9cf73211bce
Yosif Genchev <yosif.genchev@gmail.com>
2014-06-14 20:21:42 +0300

    Difficulty levels algorithm implementation, some refactoring

--- src/models/dlm/DifficultyLevel.js
+++ src/models/dlm/DifficultyLevel.js
@@ -3,6 +3,24 @@
 var Dyslexio = Dyslexio || {};
 Dyslexio.Models = Dyslexio.Models || {};
 
-Dyslexio.Models.DifficultyLevel = function (level) {
+Dyslexio.Models.DifficultyLevel = function (level, mistakes) {
   this.level = level;
+  this.mistakes = mistakes;
 };
+
+Dyslexio.Models.DifficultyLevel.prototype.getLevel = function () {
+	 return this.level;
+};
+
+Dyslexio.Models.DifficultyLevel.prototype.setLevel = function (level) {
+	 return this.level = level;
+};
+
+Dyslexio.Models.DifficultyLevel.prototype.getMistakes = function () {
+	 return this.mistakes;
+};
+
+Dyslexio.Models.DifficultyLevel.prototype.setMistakes = function (mistakes) {
+	 return this.mistakes = mistakes;
+};
+
